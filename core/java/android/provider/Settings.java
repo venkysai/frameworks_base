daada0933b8ecb7a277eba9b740727540cac0b7a
Nicholas Semendyaev <agent00791@gmail.com>
2012-09-18 20:08:36 -0400

    Navigation bar custom height
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I9ca00129929364bcbd7d6ea11b6d4220543303e4
    
    Conflicts:
    	packages/SystemUI/res/layout/navigation_bar.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2889,6 +2889,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Height of navigation bar buttons
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
