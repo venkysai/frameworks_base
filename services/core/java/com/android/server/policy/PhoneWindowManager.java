98baedb5ccd9ed24236dea0e2b84d09463f0f658
rogersb11 <brettrogers11@gmail.com>
2015-05-08 02:06:29 +0200

    Forward Port: PIE 3.0 (1/2)
    Ported to lollipop and modified by Brett Rogers(CyanideL) & BlackDragon(Fusion).
    
    PIE Key contributors:
    @BigBrother1984
    @D4rKn3sSyS
    @Anu6is
    @David96
    @drcmda
    
    P19:
       - Renamed pie directories
       - Renamed some pie class names
       - Modify copyright information
       - Use PieConstants into PieController class
    
    P20:
      fix BaseStatusBar class in reflection of P19 changes
    
    P21:
        - fix piehelper class in reference to P19
    
    P22:
       - fix piecontroller reference to P19
    
    P24:
      - fix crashes in pie control xml
    
    Note: Made to work with Marshmallow by xanaxdroid
    
    Signed-off-by: rogersb11 <brettrogesr11@gmail.com>
    Change-Id: I1838a300544bd5c769d82f1193065731baebdb48
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/custom_config.xml
    	packages/SystemUI/res/values/screwd_dimens.xml
    	packages/SystemUI/res/values/screwd_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    	services/core/java/com/android/server/statusbar/StatusBarManagerService.java
    
    Screw'd:
    Make PIE co-exist with DUI & squash fixes
    
    Fix Oversized kill task icon:
    https://github.com/ScrewdAOSP/frameworks_base/commit/78169b3cf044e1acc5dfe9efd3951b73ddcb51d0
    
    Pie: Disable background in Bare mode:
    https://github.com/ScrewdAOSP/frameworks_base/commit/141042305aedb976b7d503e6e1b128a16938cc74
    
    Pie: Do not translate Java date formats:
    https://github.com/ScrewdAOSP/frameworks_base/commit/26deb610f9aa2eece7e8dc39247d8bf9c427d424
    
    Pie: Always on the right switch by @LorDClockaN
    https://github.com/ScrewdAOSP/frameworks_base/commit/a0133eb7ed42e8354493df7a71aab1ded71ccb7f
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -606,6 +606,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // User defined bar visibility, regardless of factory configuration
     boolean mNavbarVisible = false;
 
+    // Pie
+    boolean mPieState = false;
+
     // States of keyguard dismiss.
     private static final int DISMISS_KEYGUARD_NONE = 0; // Keyguard not being dismissed.
     private static final int DISMISS_KEYGUARD_START = 1; // Keyguard needs to be dismissed.
@@ -949,6 +952,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.NAVIGATION_BAR_WIDTH), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.PA_PIE_STATE), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -1047,7 +1053,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (mStatusBar != null && !mStatusBar.isVisibleLw()) {
                 flags |= EdgeGesturePosition.TOP.FLAG;
             }
-            if (mNavigationBar != null && !mNavigationBar.isVisibleLw() && !isStatusBarKeyguard()) {
+            if (mNavigationBar != null && !mNavigationBar.isVisibleLw() && !isStatusBarKeyguard() && !immersiveModeImplementsPie()) {
                 if (mNavigationBarOnBottom) {
                     flags |= EdgeGesturePosition.BOTTOM.FLAG;
                 } else {
@@ -2178,6 +2184,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // volume rocker wake
             mVolumeRockerWake = Settings.System.getIntForUser(resolver,
                     Settings.System.VOLUME_ROCKER_WAKE, 0, UserHandle.USER_CURRENT) != 0;
+
+            // pa pie
+            mPieState = (Settings.System.getIntForUser(resolver,
+                    Settings.System.PA_PIE_STATE, 0, UserHandle.USER_CURRENT) == 1);
 
             // volume rocker music control
             mVolBtnMusicControls = (Settings.System.getIntForUser(resolver,
@@ -5243,6 +5253,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         of.bottom = cf.bottom = mUnrestrictedScreenTop + mUnrestrictedScreenHeight;
     }
 
+    private boolean immersiveModeImplementsPie() {
+        return mPieState;
+    }
+
     private void offsetInputMethodWindowLw(WindowState win) {
         int top = Math.max(win.getDisplayFrameLw().top, win.getContentFrameLw().top);
         top += win.getGivenContentInsetsLw().top;
@@ -6737,10 +6751,24 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     return;
                 }
                 if (sb) mStatusBarController.showTransient();
-                if (nb) mNavigationBarController.showTransient();
+                if (nb && !immersiveModeImplementsPie())
+                    mNavigationBarController.showTransient();
                 mImmersiveModeConfirmation.confirmCurrentPrompt();
                 updateSystemUiVisibilityLw();
             }
+        }
+    }
+
+    private void toggleOrientationListener(boolean enable) {
+        IStatusBarService statusbar = getStatusBarService();
+        if (statusbar != null) {
+           try {
+               statusbar.toggleOrientationListener(enable);
+           } catch (RemoteException e) {
+               Slog.e(TAG, "RemoteException when controlling orientation sensor", e);
+               // re-acquire status bar service next time it is needed.
+               mStatusBarService = null;
+           }
         }
     }
 
@@ -6963,6 +6991,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             } catch (RemoteException unhandled) {
             }
         }
+
+        toggleOrientationListener(true);
     }
 
     private void handleHideBootMessage() {
