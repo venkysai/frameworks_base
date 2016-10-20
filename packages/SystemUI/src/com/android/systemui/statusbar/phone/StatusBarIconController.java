5103a2201c31e8b2cf1507383c212d941b29844f
LorDClockaN <lordclockan@gmail.com>
2016-10-20 22:47:27 +0200

    CarrierLabel: Obey inverted statusbar ONLY when color is white
    Change-Id: I8a0438d33e2a77bd80edf856a254bf1b95e4efdd
    Signed-off-by: LorDClockaN <lordclockan@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -621,7 +621,13 @@ public class StatusBarIconController extends StatusBarIconList implements Tunabl
         mCenterClock.setTextColor(getTint(mTintArea, mCenterClock, mIconTint));
         mLeftClock.setTextColor(getTint(mTintArea, mLeftClock, mIconTint));
         mNetworkTraffic.setDarkIntensity(mDarkIntensity);
-        mCarrierLabel.setTextColor(getTint(mTintArea, mCarrierLabel, mIconTint));
+        if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_CARRIER_COLOR,
+                mContext.getResources().getColor(R.color.status_bar_clock_color),
+                UserHandle.USER_CURRENT) == mContext.getResources().
+                getColor(R.color.status_bar_clock_color)) {
+            mCarrierLabel.setTextColor(getTint(mTintArea, mCarrierLabel, mIconTint));
+        }
         mBatteryLevelView.setTextColor(getTint(mTintArea, mBatteryLevelView, mIconTint));
         if (mNotificationTicker != null) mNotificationTicker.setDarkIntensity(mDarkIntensity);
         if (Settings.System.getIntForUser(mContext.getContentResolver(),
