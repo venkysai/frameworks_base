4f908fb43d9cf6effcfc5e998d33ebe980a1e9ec
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-11-25 09:41:29 +0100

    Bring back portrait/landscape QS mod [1/2]
    This is just adding back part of the code that we had before
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/faa0970f4c82d4a6c0b2e925cb09f626b4baac86
    
    https://i.imgur.com/pXsr3AH.png
    
    https://i.imgur.com/5ked5gQ.png
    
    https://i.imgur.com/9YCVYty.png
    
    Change-Id: Ibe7c035c2a101f8a2300d7bb2426c1a1659b85ce
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -695,6 +695,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RECENT_CARD_TEXT_COLOR), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.QS_ROWS_PORTRAIT),
+                  false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.QS_ROWS_LANDSCAPE),
+                  false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -719,6 +725,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.STATUS_BAR_SHOW_CARRIER))) {
                 update();
                 updateCarrier();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_PORTRAIT))
+                    || uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_ROWS_LANDSCAPE))) {
+                    updateResources();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_SHOW_TICKER))) {
                 mTickerEnabled = Settings.System.getIntForUser(
