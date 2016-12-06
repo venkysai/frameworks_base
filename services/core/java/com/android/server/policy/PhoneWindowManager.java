DirtyUnicorns/android_frameworks_base

Restore part of longpress lockscreen torch code
- some code was modified for no apparent reason

Change-Id: Ifcbea04209d2f7eddf684e91316767bcd5b2e178

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1322,9 +1322,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (!performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false)) {
                     performAuditoryFeedbackForAccessibilityIfNeed();
                 }
-                boolean locked = isStatusBarKeyguard() && mKeyguardSecure;
-                boolean globalActionsOnLockScreen = Settings.System.getIntForUser(mContext.getContentResolver(),
-                            Settings.System.POWER_MENU_LOCKSCREEN, 1, UserHandle.USER_CURRENT) == 1;
+                KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
+                boolean locked = km.inKeyguardRestrictedInputMode();
+                boolean globalActionsOnLockScreen = Settings.System.getInt(
+                        mContext.getContentResolver(), Settings.System.POWER_MENU_LOCKSCREEN, 1) == 1;
                 if (locked && !globalActionsOnLockScreen) {
                     behavior = LONG_PRESS_POWER_NOTHING;
                 } else {

venkysai/frameworks_base

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1404,9 +1404,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 if (!performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false)) {
                     performAuditoryFeedbackForAccessibilityIfNeed();
                 }
-                boolean locked = isStatusBarKeyguard() && mKeyguardSecure;
-                boolean globalActionsOnLockScreen = Settings.System.getIntForUser(mContext.getContentResolver(),
-                            Settings.System.POWER_MENU_LOCKSCREEN, 1, UserHandle.USER_CURRENT) == 1;
+                KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
+                boolean locked = km.inKeyguardRestrictedInputMode();
+                boolean globalActionsOnLockScreen = Settings.System.getInt(
+                        mContext.getContentResolver(), Settings.System.LOCKSCREEN_ENABLE_POWER_MENU , 1) == 1;
                 if (locked && !globalActionsOnLockScreen) {
                     behavior = LONG_PRESS_POWER_NOTHING;
                 } else {