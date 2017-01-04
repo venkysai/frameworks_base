cac1b809819b850940f39d2f248c8f01e7a31e3d
Jon Haus <haus.xda@gmail.com>
2017-01-05 00:20:06 +0100

    Network Traffic [1/2]
    -----
    abc @ezio84:
    * Use O dark intensity API
    * Use arrows vectors from AOSP net arrows commit (thanks @Mazda-- for this find!)
    * Fix visual glitch on status bar expand (arrows showing and quickly hiding again)
    * Don't show arrows when no data connection is active
    * Remove unit settings (KB is good)
    * Remove interval (set to 1 sec by default) and single arrow options
    * Don't get settings values again when not needed
    * Fix padding between arrows/speed values and expose the padding value
    * Code cleanup
    -----
    
    Change-Id: If32bcd7764fddf00bc926b3bf6f908518b708f70
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3968,6 +3968,18 @@ public final class Settings {
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4136,6 +4148,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(WAKE_WHEN_PLUGGED_OR_UNPLUGGED);
             PRIVATE_SETTINGS.add(SWAP_VOLUME_BUTTONS);
             PRIVATE_SETTINGS.add(VOLUME_KEYS_CONTROL_MEDIA_STREAM);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
         }
 
         /**
