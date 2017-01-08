c4e1d85b3e1555e257632cbacb625d8147903818
maxwen <max.weninger@gmail.com>
2017-01-08 20:47:49 +0100

    [1/3] base: custom header extensions
    add static image provider that allows using a
    specific image from a header pack
    
    Screw'd:
    Add missing stuffz to make it work!!!
    
    Change-Id: Ie695aa9c39f8a61594c72bcd450e3cdfc02ba2c1
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -494,8 +494,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
     @Override
     public void updateSettings() {
         if (mQsPanel != null) {
-            mQsPanel.updateSettings();
-
             // if header is active we want to push the qs panel a little bit further down
             // to have more space for the header image
             post(new Runnable() {
@@ -508,9 +506,6 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
                     mQsPanel.setLayoutParams(params);
                 }
             });
-        }
-        if (mHeaderQsPanel != null) {
-            mHeaderQsPanel.updateSettings();
         }
         applyHeaderBackgroundShadow();
     }
