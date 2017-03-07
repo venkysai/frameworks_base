ddb79e1b250c73bd8a0a30e308ec1f3902134970
Corey Edwards <ensabahnur16@gmail.com>
2017-03-07 15:53:52 -0600

    Revert "Add a protected broadcast for Masquerade events"
    This reverts commit 9c3bc75fd106b9c8cadcb4b284d1132d940337b8.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4466,8 +4466,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                         || actionName.startsWith("eu.chainfire.supersu.NativeAccess")
                         || actionName.startsWith("intent_navbar_edit")
                         || actionName.startsWith("qualcomm.intent.action.ACTION_AUDIO_STATE_CHANGED")
-                        || actionName.startsWith("com.qualcomm.intent.action.ACTION_UNSOL_RESPONSE_OEM_HOOK_RAW")
-                        || actionName.startsWith("masquerade.substratum.STATUS_CHANGED")) {
+                        || actionName.startsWith("com.qualcomm.intent.action.ACTION_UNSOL_RESPONSE_OEM_HOOK_RAW")) {
                     return true;
                 }
             }
