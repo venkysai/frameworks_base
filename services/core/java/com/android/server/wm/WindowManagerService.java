af111fd80deb4aa15bbdbc96c79a9e445c3ae8e9
ezio84 <brabus84@gmail.com>
2017-03-24 10:15:03 +0100

    Build Slim Recents into SystemUI [1/2]
    track packages/apps/SlimRecents tree and pick the Settings commits
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -205,6 +205,7 @@ import static android.view.WindowManager.LayoutParams.TYPE_NAVIGATION_BAR;
 import static android.view.WindowManager.LayoutParams.TYPE_PHONE;
 import static android.view.WindowManager.LayoutParams.TYPE_PRIVATE_PRESENTATION;
 import static android.view.WindowManager.LayoutParams.TYPE_QS_DIALOG;
+import static android.view.WindowManager.LayoutParams.TYPE_SLIM_RECENTS;
 import static android.view.WindowManager.LayoutParams.TYPE_STATUS_BAR;
 import static android.view.WindowManager.LayoutParams.TYPE_TOAST;
 import static android.view.WindowManager.LayoutParams.TYPE_VOICE_INTERACTION;
@@ -1884,6 +1885,7 @@ public class WindowManagerService extends IWindowManager.Stub
             case TYPE_NAVIGATION_BAR:
             case TYPE_INPUT_METHOD_DIALOG:
             case TYPE_PHONE:
+            case TYPE_SLIM_RECENTS:
                 return true;
         }
         return false;
