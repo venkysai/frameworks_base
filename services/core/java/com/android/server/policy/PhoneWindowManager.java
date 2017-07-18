ca0255a2b4f7c1b8a2e6276795edc865508eeb35
maxwen <max.weninger@gmail.com>
2017-07-18 20:34:37 +0200

    base: SystemUI: add OmniSwitch hide on home
    Change-Id: If21bccc3c6f8116a21508a2634c3ece25c100311
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3794,6 +3794,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // Remember that home is pressed and handle special actions.
             if (repeatCount == 0) {
                 mHomePressed = true;
+                if (mOmniSwitchRecents) {
+                    screwdUtils.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
+                }
                 if (mHomeDoubleTapPending) {
                     mHomeDoubleTapPending = false;
                     mHandler.removeCallbacks(mHomeDoubleTapTimeoutRunnable);
