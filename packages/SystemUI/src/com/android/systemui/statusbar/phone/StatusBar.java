bfc96c8bdf4cefa2eec94c99068a66906a1f898a
maxwen <max.weninger@gmail.com>
2017-10-12 20:24:01 +0200

    FWB: add status bar logo (1/2)
    Change-Id: I86eb407557197229481940e565ab711eb56312b0
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5845,6 +5845,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         }
 
+        @Override
+            public void onChange(boolean selfChange) {
+        }
+
         public void update() {
             setDoubleTapNavbar();
             setStatusBarWindowViewOptions();
