dbdd84b80510f955c6335c7b543239e52a7253bc
Corey Edwards <ensabahnur16@gmail.com>
2018-01-12 21:24:35 -0600

    Revert "SEEMP: rename optional app verifier intent name"
    This reverts commit 7c023b501714580ace0f895e57d8051216b54ef4.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/accounts/AccountManager.java
+++ core/java/android/accounts/AccountManager.java
@@ -2582,8 +2582,8 @@ public class AccountManager {
         @Override
         public void doWork() throws RemoteException {
             android.util.SeempLog.record(31);
-            getAccountByTypeAndFeatures(mAccountType, mFeatures,
-                    new AccountManagerCallback<Bundle>() {
+            getAccountsByTypeAndFeatures(mAccountType, mFeatures,
+                    new AccountManagerCallback<Account[]>() {
                         @Override
                         public void run(AccountManagerFuture<Bundle> future) {
                             String accountName = null;
