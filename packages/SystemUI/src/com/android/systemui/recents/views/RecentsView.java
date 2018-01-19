36239719f592fcfe6314b194a7c21c1853fec1b5
Corey Edwards <ensabahnur16@gmail.com>
2018-01-19 08:47:33 -0600

    Revert "SystemUI: Recents: Allow closing empty view via tapping"
    This reverts commit 6ff4e56c252c7c42b24a151d86aff2d630535bff.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -78,7 +78,6 @@ import com.android.systemui.recents.events.activity.LaunchTaskEvent;
 import com.android.systemui.recents.events.activity.MultiWindowStateChangedEvent;
 import com.android.systemui.recents.events.activity.ShowEmptyViewEvent;
 import com.android.systemui.recents.events.activity.ShowStackActionButtonEvent;
-import com.android.systemui.recents.events.activity.ToggleRecentsEvent;
 import com.android.systemui.recents.events.component.ExpandPipEvent;
 import com.android.systemui.recents.events.ui.AllTaskViewsDismissedEvent;
 import com.android.systemui.recents.events.ui.DismissAllTaskViewsEvent;
@@ -417,12 +416,6 @@ public class RecentsView extends FrameLayout {
         if (RecentsDebugFlags.Static.EnableStackActionButton) {
             mStackActionButton.bringToFront();
         }
-        setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EventBus.getDefault().send(new ToggleRecentsEvent());
-            }
-        });
     }
 
     /**
@@ -435,7 +428,6 @@ public class RecentsView extends FrameLayout {
         if (RecentsDebugFlags.Static.EnableStackActionButton) {
             mStackActionButton.bringToFront();
         }
-        setOnClickListener(null);
     }
 
     /**
@@ -641,11 +633,7 @@ public class RecentsView extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-        if (mTouchHandler.onTouchEvent(ev)) {
-            return true;
-        } else {
-            return super.onTouchEvent(ev);
-        }
+        return mTouchHandler.onTouchEvent(ev);
     }
 
     @Override
