b13bb736a92eb4fea28cb0a19601df0ddc46fd0b
Rabih Salamey <somcom3@gmail.com>
2018-01-27 22:15:12 -0500

    Add partial screenshot to the screenshot QS tile
    - Single tap to choose the mode, long press to fire
    
    Vectors redone by @Mazda-- , see image below
    https://i.imgur.com/4rKXmAv.png
    
    Change-Id: I2d88286209179109a86be63ba92cd546eb4baff0
    Signed-off-by: TheHungarianHorntail <sayan7848@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenshotTile.java
@@ -27,6 +27,7 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
+import android.os.UserHandle;
 import android.view.View;
 
 import com.android.systemui.R;
@@ -38,6 +39,8 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 /** Quick settings tile: Screenshot **/
 public class ScreenshotTile extends QSTileImpl<BooleanState> {
+
+    private boolean mRegion = false;
 
     private boolean mListening;
     private final Object mScreenshotLock = new Object();
@@ -60,14 +63,8 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleClick() {
-        mHost.collapsePanels();
-        /* wait for the panel to close */
-        try {
-             Thread.sleep(1500);
-        } catch (InterruptedException ie) {
-             // Do nothing
-        }
-        takeScreenshot();
+        mRegion = !mRegion;
+        refreshState();
     }
 
     @Override
@@ -79,7 +76,7 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         } catch (InterruptedException ie) {
              // Do nothing
         }
-        takeScreenshot();
+        takeScreenshot(mRegion ? 2 : 1);
     }
 
     @Override
@@ -89,10 +86,17 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        state.label = mContext.getString(R.string.quick_settings_screenshot_label);
-        state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
-        state.contentDescription =  mContext.getString(
-                   R.string.quick_settings_screenshot_label);
+        if (mRegion) {
+            state.label = mContext.getString(R.string.quick_settings_region_screenshot_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_region_screenshot);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_region_screenshot_label);
+        } else {
+            state.label = mContext.getString(R.string.quick_settings_screenshot_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_screenshot);
+            state.contentDescription =  mContext.getString(
+                    R.string.quick_settings_screenshot_label);
+        }
     }
 
     @Override
@@ -105,9 +109,9 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         return MetricsEvent.SCREWD;
     }
 
+
     final Runnable mScreenshotTimeout = new Runnable() {
-        @Override
-        public void run() {
+        @Override public void run() {
             synchronized (mScreenshotLock) {
                 if (mScreenshotConnection != null) {
                     mContext.unbindService(mScreenshotConnection);
@@ -117,13 +121,15 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
         }
     };
 
-    private void takeScreenshot() {
+    private void takeScreenshot(final int screenshotType) {
         synchronized (mScreenshotLock) {
             if (mScreenshotConnection != null) {
                 return;
             }
-
-            Intent intent = new Intent(mContext, TakeScreenshotService.class);
+            ComponentName cn = new ComponentName("com.android.systemui",
+                    "com.android.systemui.screenshot.TakeScreenshotService");
+            Intent intent = new Intent();
+            intent.setComponent(cn);
             ServiceConnection conn = new ServiceConnection() {
                 @Override
                 public void onServiceConnected(ComponentName name, IBinder service) {
@@ -131,9 +137,8 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
                         if (mScreenshotConnection != this) {
                             return;
                         }
-
                         Messenger messenger = new Messenger(service);
-                        Message msg = Message.obtain(null, 1);
+                        Message msg = Message.obtain(null, screenshotType);
                         final ServiceConnection myConn = this;
                         Handler h = new Handler(mHandler.getLooper()) {
                             @Override
@@ -158,14 +163,11 @@ public class ScreenshotTile extends QSTileImpl<BooleanState> {
                         }
                     }
                 }
-
                 @Override
-                public void onServiceDisconnected(ComponentName name) {
-                    // Do nothing here
-                }
+                public void onServiceDisconnected(ComponentName name) {}
             };
-
-            if (mContext.bindService(intent, conn, mContext.BIND_AUTO_CREATE)) {
+            if (mContext.bindServiceAsUser(
+                    intent, conn, Context.BIND_AUTO_CREATE, UserHandle.CURRENT)) {
                 mScreenshotConnection = conn;
                 mHandler.postDelayed(mScreenshotTimeout, 10000);
             }
