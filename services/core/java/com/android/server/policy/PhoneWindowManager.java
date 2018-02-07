245506430e95b4ba909c097d840aa7d2d6a1761e
mydongistiny <jaysonedson@gmail.com>
2018-02-07 03:20:51 -0800

    fwb: Set initial development settings enabled value
    PixelLauncher doesn't seem to be picking up development settings at first boot since it's enabled by default and you have to toggle it once before the icon shape option shows up. Enable it here so at first boot it will detect it.
    
    Change-Id: I11d92ea0a5bf7dbc95fbcc30e36918b010b26e43
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1102,10 +1102,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ACCELEROMETER_ROTATION_ANGLES), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false, this,
+                    UserHandle.USER_ALL);
             updateSettings();
         }
 
-        @Override public void onChange(boolean selfChange) {
+        @Override
+        public void onChange(boolean selfChange) {
             updateSettings();
             updateRotation(false);
         }
@@ -2372,6 +2376,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mGlobalKeyManager = new GlobalKeyManager(mContext);
 
+        setDevelopmentSettings();
+
         // Controls rotation and the like.
         initializeHdmiState();
 
@@ -2652,6 +2658,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
         if (updateRotation) {
             updateRotation(true);
+        }
+        setDevelopmentSettings();
+    }
+
+    private void setDevelopmentSettings() {
+        int developmentSettingsEnabled = Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 1);
+        if (developmentSettingsEnabled == 1) {
+            Slog.i(TAG, "Enabled Development Settings.");
+            Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 1);
+        } else {
+            Slog.i(TAG, "Disabled Development Settings.");
+            Settings.Global.putInt(mContext.getContentResolver(),
+                Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0);
+
         }
     }
 
@@ -4290,7 +4312,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void takeBugreport() {
         if ("1".equals(SystemProperties.get("ro.debuggable"))
                 || Settings.Global.getInt(mContext.getContentResolver(),
-                        Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) == 1) {
+                        Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 1) == 1) {
             try {
                 ActivityManager.getService()
                         .requestBugReport(ActivityManager.BUGREPORT_OPTION_INTERACTIVE);
